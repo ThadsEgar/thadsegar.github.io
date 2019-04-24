d0ed0550f30753aff489c2ed4d65ede1b2ad4607
Thads Egar <tmichaelegar@gmail.com>
2019-04-23 23:13:48 -0500

    Updates

---
+++ precache-manifest.eac095bbf3090bc61cff0c277545c9fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ec908751d659c5c06b89",
+    "url": "/static/js/main.047f49d9.chunk.js"
+  },
+  {
+    "revision": "8589e8d2fb7d58a2e39f",
+    "url": "/static/js/2.5a4c8a6f.chunk.js"
+  },
+  {
+    "revision": "ec908751d659c5c06b89",
+    "url": "/static/css/main.6e1cb661.chunk.css"
+  },
+  {
+    "revision": "8480744e2128d3c67fe74bd0180872c5",
+    "url": "/index.html"
+  }
+];
